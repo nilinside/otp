bb6bfd7cc89bb4ebb43a0a2a49d58a52bbc0749b
Murphy <murphy@nilinside.com>
2019-09-11 21:08:15 +0800

    otpmization

--- yubikey/yubikey.go
+++ yubikey/yubikey.go
@@ -3,6 +3,8 @@ package yubikey
 import (
 	"encoding/hex"
 	"github.com/conformal/yubikey"
+	"github.com/nilinside/otp"
+	"net/url"
 )
 
 type Validator struct {
@@ -32,11 +34,11 @@ func (v *Validator) Validate(passcode string) bool {
 }
 
 func (v *Validator) getToken(otpString string, priv *yubikey.Key) error {
-	_, otp, err := yubikey.ParseOTPString(otpString)
+	_, o, err := yubikey.ParseOTPString(otpString)
 	if err != nil {
 		return err
 	}
-	t, err := otp.Parse(*priv)
+	t, err := o.Parse(*priv)
 	if err != nil {
 		return err
 	}
@@ -52,6 +54,41 @@ func (v *Validator) getSecretKey(key string) (*yubikey.Key, error) {
 	priv := yubikey.NewKey(b)
 
 	return &priv, nil
+}
+
+//todo::generate keyUrl
+type GenerateOpts struct {
+	// Name of the issuing Organization/Company.
+	Issuer string
+	// Name of the User's Account (eg, email address)
+	AccountName string
+	// Secret to store. Defaults to a randomly generated secret of SecretSize.  You should generally leave this empty.
+	Secret string
+	// Digits to request. Defaults to 6.
+	Digits otp.Digits
+	// Algorithm to use for HMAC. Defaults to SHA1.
+	Algorithm otp.Algorithm
+}
+
+func Generate(opts GenerateOpts) (*otp.Key, error) {
+	// url encode the Issuer/AccountName
+	if opts.Issuer == "" {
+		return nil, otp.ErrGenerateMissingIssuer
+	}
+
+	if opts.AccountName == "" {
+		return nil, otp.ErrGenerateMissingAccountName
+	}
+	v := url.Values{}
+	v.Set("issuer", opts.Issuer)
+	v.Set("secret", opts.Secret)
+	u := url.URL{
+		Scheme:   "otpauth",
+		Host:     "yubikey",
+		Path:     "/" + opts.Issuer + ":" + opts.AccountName,
+		RawQuery: v.Encode(),
+	}
+	return otp.NewKeyFromURL(u.String())
 }
 
 func NewValidator(secret string, counter uint64, use uint64) *Validator {
